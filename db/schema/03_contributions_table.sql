dd0aaf3b4bd0e03d5481fddd00f7d2711a6622fd
Kamila Lemaire <kb.lemaire@gmail.com>
2023-05-08 10:38:28 -0600

    Create contributions table and seeds for that table, as well as index.html to work on how the contributions will look on the app

---
+++ db/schema/03_contributions_table.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS contributions CASCADE;
+CREATE TABLE contributions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+  story_id INTEGER REFERENCES stories(id) ON DELETE CASCADE
+  content TEXT
+  status BOOLEAN DEFAULT FALSE
+  num_of_upvotes INTEGER
+);
+
