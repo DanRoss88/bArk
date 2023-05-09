b3358fa0e4b61c02ec568f56c8aefd3f4e9532d7
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-09 13:28:34 -0700

    Update 01_users.sql

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,4 +1,4 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, username, email, password) VALUES ('Dob Herman', 'dobermann', 'bigdog1@example.com', 'password1@1');
-INSERT INTO users (name, username, email, password) VALUES ('Sam Oya', 'samoyed', 'reallyfluffy@example.com', 'password1@1');
-INSERT INTO users (name, username, email, password) VALUES ('Rottie Wild', 'rottweiler', 'bigdog2@example.com', 'password1@1');
+INSERT INTO users (name, username, image, email, password) VALUES ('Dob Herman', 'dobermann', '', 'bigdog1@example.com', 'password1@1');
+INSERT INTO users (name, username, image, email, password) VALUES ('Sam Oya', 'samoyed', '', 'reallyfluffy@example.com', 'password1@1');
+INSERT INTO users (name, username, image, email, password) VALUES ('Rottie Wild', 'rottweiler', '', 'bigdog2@example.com', 'password1@1');
