4a8eb0495ca5ff5d0b1f3d5e2af3b02ef2d3ffeb
Kamila Lemaire <kb.lemaire@gmail.com>
2023-05-11 14:18:28 -0600

    tidying up unused code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -20,6 +20,7 @@ let $editButton = $('.editing-form').hide();
 $('#edit-slide-button').click(() => {
   $editButton.slideToggle('slow','swing').focus();
 });
+});
 
 
 
@@ -75,73 +76,6 @@ $('#edit-slide-button').click(() => {
 //     }
 //   };
 // });
-
-// CREATE Contribution Element
-
-// const createContributionElement = (contribution) => {
-//   const $contributionElement = $(`
-//   <article class="contributions-container">
-//               <header class="contributions-header">
-//                 <div class="user-handle">
-//                   <p>${contribution.user_id}</p>
-//                 </div>
-//               </header>
-//               <p class="contribution-content">${contribution.content}</p>
-//               <footer class="contribution-footer">
-//                 <div class="accepted-status">
-//                 <p>${contribution.accepted_status} </p>
-//                 </div>
-//                 <div class="num-of-upvotes">${contribution.num_of_upvotes}</div>
-//                   <i class="fa-solid fa-angles-down"></i>
-//                 </div>
-//               </footer>
-//             </article>
-//   `);
-
-//   return $contributionElement;
-}
-
-  // RENDER Contributions
-
-  // const renderContributions = (contributions) => {
-
-  //   const $contributionSection = $('#contributions-container').empty();
-
-  //   for (const contribution of contributions) {
-  //     const $contributionElement = createContributionElement(contribution);
-  //     $contributionSection.prepend($contributionElement);
-  //   }
-  // };
-
-
-
-
-// LOAD Contributions
-// const loadContributions = () => {
-//   $.ajax({
-//     method: 'GET',
-//     url:'/contributions'
-//   }).then ((contributions) => {
-//     renderContributions(contributions);
-//   });
-// };
-// loadContributions();
-
-
-
-// });
-
-// // LOAD Contributions
-// const loadContributions = () => {
-//   $.ajax({
-//     method: 'GET',
-//     url:'/contributions'
-//   }).then ((contributions) => {
-//     renderContributions(contributions);
-//   });
-// };
-// loadContributions();
-
 
 
 ////************* LOAD STORIES **************////
