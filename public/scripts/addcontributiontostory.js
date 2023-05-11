e77f27c28403f80ce069b86f76afcfe7c6c32142
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-11 14:29:07 -0700

    Update addcontributiontostory.js

--- public/scripts/addcontributiontostory.js
+++ public/scripts/addcontributiontostory.js
@@ -1,16 +1,10 @@
 const { addContributionToStory } = require('../db/queries/stories');
 $(document).ready(function() {
 
-$(".publish-button").on('click', function() {
-     try {
+$(".accept-contribution").on('click', function() {
      const storyID = req.params.id;
      const { content } = req.body;
-     await addContributionToStory(storyID, content);
-     res.redirect(`/stories/${storyID}`);
-   } catch (err) {
-     console.error(err);
-     res.status(500).send('Server error');
-   }
+     addContributionToStory(storyID, content);
 });
 
 });
