a566adea732f9be80786e55ede7d6d4139726591
Kamila Lemaire <kb.lemaire@gmail.com>
2023-05-12 08:29:56 -0600

    Updating contribution seeds to match stories seeds

--- db/seeds/01_contribution_posts.sql
+++ db/seeds/01_contribution_posts.sql
@@ -1,6 +1,7 @@
 
-INSERT INTO contributions (user_id, story_id, content, accepted_status, num_of_upvotes) VALUES (3, 1, 'After completing all of his tricks, Dob was given four whole treats', 'false', 0);
-INSERT INTO contributions (user_id, story_id, content, accepted_status, num_of_upvotes) VALUES (1, 2, 'Because he was so fluffy, Sam was considered to be the best snuggler and the goodest boy', 'false', 0);
-INSERT INTO contributions (user_id, story_id, content, accepted_status, num_of_upvotes) VALUES (2, 3, 'Rottie looked like a big tough guard dog, but really, she was the biggest sweetheart', 'false', 0);
+INSERT INTO contributions (user_id, story_id, content, accepted_status, num_of_upvotes) VALUES (3, 2, 'Suddenly, Bella stopped in front of a man carrying a large briefcase. He looked nervously at her and then noticed Officer Gomez. Before Officer Gomez could react, the man took off, with Bella giving chase.', 'false', 0);
+INSERT INTO contributions (user_id, story_id, content, accepted_status, num_of_upvotes) VALUES (1, 3, 'Could Max have been just the friend that Ben needed all along?', 'false', 0);
+INSERT INTO contributions (user_id, story_id, content, accepted_status, num_of_upvotes) VALUES (2, 4, 'The sun was starting to set, which would make the search must more dangerous, but Sarah knew she had to do everything that she could to help. One look at Rocky and she knew he felt the same way.', 'false', 0);
+
 
 
